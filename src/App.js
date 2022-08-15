be9e0749acd92b83e187b57fad66be0c75e5a2a9
Thai Phan <tvphan360@gmail.com>
2022-08-15 18:16:57 -0400

    add carousel code challenge

--- src/App.js
+++ src/App.js
@@ -1,23 +1,65 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React from "react";
 
 function App() {
+  const [slides, setSlides] = React.useState([]);
+  const [currentSlide, setCurrentSlide] = React.useState(0);
+
+  React.useEffect(() => {
+    // simulate async api call
+    new Promise((resolve) => {
+      setTimeout(() => {
+        resolve([
+          "http://placekitten.com/g/300/300",
+          "http://placekitten.com/g/300/300",
+          "http://placekitten.com/g/300/300",
+          "http://placekitten.com/g/300/300",
+          "http://placekitten.com/g/300/300",
+        ]);
+      }, 1000);
+    }).then((result) => {
+      // hydrate the carousel with data
+      // todo: loading state
+      setSlides(result);
+    });
+  }, []);
+
+  const handlePrev = () => {
+    if (currentSlide > 0) {
+      setCurrentSlide(currentSlide - 1);
+    }
+  };
+
+  const handleNext = () => {
+    if (currentSlide < slides.length - 1) {
+      setCurrentSlide(currentSlide + 1);
+    }
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+      <div id="myCarousel">
+        <div
+          id="myCarousel-inner"
+          style={{
+            transform: `translate(calc((-300px - 24px) * ${currentSlide}))`,
+          }}
         >
-          Learn React
-        </a>
-      </header>
+          {slides.map((slide, index) => (
+            <article key={index}>
+              <img src={slide} />
+            </article>
+          ))}
+        </div>
+      </div>
+      <div class="button-row">
+        <button type="button" class="prev" onClick={handlePrev}>
+          Prev
+        </button>
+        <button type="button" class="next" onClick={handleNext}>
+          Next
+        </button>
+      </div>
     </div>
   );
 }
